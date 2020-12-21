2bc5c05c32c5f94b01277a5462ecc51dee03388f
esmasher <esmasherr@gmail.com>
2020-12-20 19:05:29 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 3, "M", "brown", "happy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 1, "F", "black", "nervous", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 2, "F", "white", "sad", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergent Brown", 7, "M", "brown", "angry", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 6, "M", "black", "happy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 9, "M", "green", "grouchy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 3, "F", "white", "happy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 5, "M", "black", "excited", 1);
